ff3565aa4d299aaa366bca9e2c45f5835a12b89e
Mark Shieh <mark@skurt.com>
2018-01-04 15:04:56 -0800

    format for serialization

--- backend/reddit/rest/topic/resources.py
+++ backend/reddit/rest/topic/resources.py
@@ -10,15 +10,15 @@ class TopicListResource(ListResource):
     def get(self):
         resp = [
             {
-                'id': topic.id,
+                'id': str(topic.id),
                 'title': topic.title,
-                'user_id': topic.user_id,
-                'num_upvotes': topic.num_upvotes,
-                'num_downvotes': topic.num_downvotes,
-                'hotness_score': topic.hotness_score,
-                'created_by_id': topic.created_by_id,
-                'created_at': topic.created_at,
-                'updated_at': topic.updated_at
+                'user_id': str(topic.user_id),
+                'num_upvotes': int(topic.num_upvotes),
+                'num_downvotes': int(topic.num_downvotes),
+                'hotness_score': float(topic.hotness_score),
+                'created_by_id': str(topic.created_by_id),
+                'created_at': topic.created_at.isoformat(),
+                'updated_at': topic.updated_at.isoformat(),
             } for topic in self.db.query(Topic).all()
         ]
         return Response(json.dumps(resp), mimetype='application/json')
